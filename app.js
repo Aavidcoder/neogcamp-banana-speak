97fe85571b45d58d9fde191b7a86f82127320942
Vikrant Patil <pvikrant248@gmail.com>
2023-03-27 01:56:38 +0530

    taking input and consoling it

---
+++ app.js
@@ -0,0 +1,12 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+
+
+function clickHandler(){
+    console.log("clicked");
+    console.log(textInput.value);
+}
+
+
+btnTranslate.addEventListener("click",clickHandler);
+
