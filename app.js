a21bdb939f8657ff3d84e0d4e2e89af885b72212
Vikrant Patil <pvikrant248@gmail.com>
2023-03-28 13:45:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,32 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
 
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl(text){
+    return serverUrl + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+}
+
 
 
 function clickHandler(){
-    // console.log("clicked");
-    // console.log(textInput.value);
-    outputDiv.innerText = "Translate: " + textInput.value;
 
-}
+    var inputText = textInput.value;
+
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+             outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+
+
+};
 
 
 btnTranslate.addEventListener("click",clickHandler);
